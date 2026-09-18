f08c32bacc8c017fed8de63d4ea076daad5422aa
garg-anant205 <anant.garg@kinesthetiq.com>
2026-09-18 11:47:24 +0530

    updated torch codec script and GPU arch

--- scripts/torchcodec_example.py
+++ scripts/torchcodec_example.py
@@ -1,20 +1,39 @@
 #! /usr/bin/env python3
+
 import os
 import sys
+
+import random
+import time
+from tqdm import tqdm
+
+import numpy as np
+import torch
 from torchcodec.decoders import VideoDecoder
 
 if __name__ == "__main__":
 
-    decoder = VideoDecoder(
+    dec = VideoDecoder(
         sys.argv[1],
         device="cuda",
     )
 
-    L = len(decoder)
-    print(L)
+    T = []
 
-    frame = decoder[0]
+    for _ in tqdm(range(100)):
+        idx = random.randint(0, len(dec) - 1)
 
-    print(frame.type)
-    print(frame.shape)
-    print(frame.device)
+        t0 = time.perf_counter()
+        frame = dec[idx]
+        # print(frame.device) # prints cuda
+        torch.cuda.synchronize()
+        dt = time.perf_counter() - t0
+
+        # print(idx, dt)
+        T.append(dt)
+
+    T = np.array(T)
+    print(T.mean())
+    print(T.std())
+    print(T.min())
+    print(T.max())
