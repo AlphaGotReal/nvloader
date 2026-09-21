f6a87d9bc2d39f13332ebfff13912d42a088f5ba
AlphaGotReal <subhampatra2411@gmail.com>
2026-09-21 15:34:39 +0530

    untested manifest generator

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ import sys
 from IPython import embed
 import argparse
 
+from nvloader import manifest
+
 def main(args):
-    return 
+    return manifest.generate(args.output)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--cont", action="store_true")
     parser.add_argument("--gvar", default="gvar")
+    parser.add_argument("--output")
     args = parser.parse_args()
 
     _gvar = args.gvar
